5484b9c640f57378122c0258df0edff6a7fd5b83
stfn-glvgn <stefanoskater1998@gmail.com>
2023-03-05 10:17:14 +0100

    First commit

---
+++ modules/resnet.py
@@ -0,0 +1,162 @@
+import torch.nn as nn
+import torch
+from torchvision.models.resnet import Bottleneck, BasicBlock, conv1x1
+
+
+class ResNet(nn.Module):
+
+    def __init__(self, block, layers, num_classes=1000, zero_init_residual=False,
+                 groups=1, width_per_group=64, replace_stride_with_dilation=None,
+                 norm_layer=None):
+        super(ResNet, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        self._norm_layer = norm_layer
+
+        self.inplanes = 64
+        self.dilation = 1
+        if replace_stride_with_dilation is None:
+            # each element in the tuple indicates if we should replace
+            # the 2x2 stride with a dilated convolution instead
+            replace_stride_with_dilation = [False, False, False]
+        if len(replace_stride_with_dilation) != 3:
+            raise ValueError("replace_stride_with_dilation should be None "
+                             "or a 3-element tuple, got {}".format(replace_stride_with_dilation))
+        self.groups = groups
+        self.base_width = width_per_group
+        self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2, padding=3,
+                               bias=False)
+        self.bn1 = norm_layer(self.inplanes)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2,
+                                       dilate=replace_stride_with_dilation[0])
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2,
+                                       dilate=replace_stride_with_dilation[1])
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2,
+                                       dilate=replace_stride_with_dilation[2])
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.rep_dim = 512 * block.expansion
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+        # Zero-initialize the last BN in each residual branch,
+        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
+        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
+        if zero_init_residual:
+            for m in self.modules():
+                if isinstance(m, Bottleneck):
+                    nn.init.constant_(m.bn3.weight, 0)
+                elif isinstance(m, BasicBlock):
+                    nn.init.constant_(m.bn2.weight, 0)
+
+    def _make_layer(self, block, planes, blocks, stride=1, dilate=False):
+        norm_layer = self._norm_layer
+        downsample = None
+        previous_dilation = self.dilation
+        if dilate:
+            self.dilation *= stride
+            stride = 1
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                conv1x1(self.inplanes, planes * block.expansion, stride),
+                norm_layer(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample, self.groups,
+                            self.base_width, previous_dilation, norm_layer))
+        self.inplanes = planes * block.expansion
+        for _ in range(1, blocks):
+            layers.append(block(self.inplanes, planes, groups=self.groups,
+                                base_width=self.base_width, dilation=self.dilation,
+                                norm_layer=norm_layer))
+
+        return nn.Sequential(*layers)
+
+    def _forward_impl(self, x):
+        # See note [TorchScript super()]
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+
+        return x
+
+    def forward(self, x):
+        return self._forward_impl(x)
+
+
+def get_resnet(name):
+    resnet18 = ResNet(block=BasicBlock, layers=[2, 2, 2, 2])
+    resnet34 = ResNet(block=BasicBlock, layers=[3, 4, 6, 3])
+    resnet50 = ResNet(block=Bottleneck, layers=[3, 4, 6, 3])
+
+    #resnet18_HAR = ResNet(block=BasicBlock, layers=[2, 2, 2, 2], low_dim)
+
+    resnets = {
+        "ResNet18": resnet18,
+        "ResNet34": resnet34,
+        "ResNet50": resnet50,
+    }
+    if name not in resnets.keys():
+        raise KeyError(f"{name} is not a valid ResNet version")
+    return resnets[name]
+
+
+
+
+
+##########################################################################################################################################################################
+##########################################################################################################################################################################
+##########################################################################################################################################################################
+
+
+from torchvision.models import resnet
+
+
+def ResNet18TS(low_dim=128, dataset_name='wisdm'):
+    if dataset_name == 'HAR':
+        in_channels = 9
+    elif dataset_name == 'SHAR':
+        in_channels = 3
+    elif dataset_name == 'wisdm':
+        in_channels = 3
+    elif dataset_name == 'epilepsy':
+        in_channels = 1
+    elif dataset_name == 'FingerMovements':
+        in_channels = 28
+    elif dataset_name == 'PenDigits':
+        in_channels = 2
+    elif dataset_name == 'EigenWorms':
+        in_channels = 6
+
+    net = resnet.ResNet(resnet.BasicBlock, [2, 2, 2, 2], low_dim)
+    if dataset_name=='wisdm':
+        net.conv1 = nn.Conv2d(in_channels, 64, kernel_size=8, stride=1, padding=4, bias=False)
+    elif dataset_name=='HAR':
+        net.conv1 = nn.Conv2d(9, 64, kernel_size=8, stride=1, padding=4, bias=False)
+    elif dataset_name=='epilepsy':
+        net.conv1 = nn.Conv2d(1, 64, kernel_size=8, stride=1, padding=43, bias=False)
+    elif dataset_name == 'SHAR':
+        net.conv1 = nn.Conv2d(3, 64, kernel_size=8, stride=1, padding=4, bias=False)
+    else:
+        net.conv1 = nn.Conv2d(in_channels, 64, kernel_size=7, stride=2, padding=3, bias=False)
+
+    net.maxpool = nn.Identity()
+
+    return net
